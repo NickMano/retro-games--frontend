8687f6af1d9a0be3c37c40b942bf5fa782b57a2f
Nico <nmanograsso@gmail.com>
2020-04-05 14:03:19 -0300

    Add logic to login form

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,34 +1,62 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../assets/styles/components/Login.scss'
 import { Link } from 'react-router-dom'
  
-const Login = () => (
-    <>
-        <section className="login">
-            <section className="login__container">
-                <h2 className="login__container--title">Inicia Sesion</h2>
-                <form className="login__container--form">
-                    <input type="text" className="login__container--input" placeholder="Correo" />
-                    <input type="password" className="login__container--input" placeholder="Contraseña" />
-                    <button className="login__container--button">Iniciar sesion</button>
-                    <div className="login__container--remember-me">
-                        <div>
-                            <input type="checkbox" id="cbox1" />
-                            <label>Recuerdame</label>
+const Login = () => {
+    const [form, setValues] = useState({ email : ''})
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+        console.log(form)
+    }
+    return(
+        <>
+            <section className="login">
+                <section className="login__container">
+                    <h2 className="login__container--title">Inicia Sesion</h2>
+                    <form className="login__container--form" onSubmit={handleSubmit}>
+                        <input 
+                            type="text" 
+                            className="login__container--input" 
+                            name="email"
+                            placeholder="Correo" 
+                            onChange={handleInput}
+                        />
+                        <input
+                            name="password" 
+                            type="password" 
+                            className="login__container--input" 
+                            placeholder="Contraseña" 
+                            onChange={handleInput}
+
+                        />
+                        <button className="login__container--button">Iniciar sesion</button>
+                        <div className="login__container--remember-me">
+                            <div>
+                                <input type="checkbox" id="cbox1" />
+                                <label>Recuerdame</label>
+                            </div>
+                            <a href="#">Olvide mi contraseña</a>
                         </div>
-                        <a href="#">Olvide mi contraseña</a>
-                    </div>
-                </form>
-                <section className="login__container--social-media">
-                    <div><img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Google_icon-icons.com_66793.png" alt="Google" />Inicia sesión con Google</div>
-                    <div><img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Twitter_icon-icons.com_66803.png" alt="Twitter"/>Inicia sesión con Twitter</div>
+                    </form>
+                    <section className="login__container--social-media">
+                        <div><img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Google_icon-icons.com_66793.png" alt="Google" />Inicia sesión con Google</div>
+                        <div><img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Twitter_icon-icons.com_66803.png" alt="Twitter"/>Inicia sesión con Twitter</div>
+                    </section>
+                    <p className="login__container--register">No tienes ninguna cuenta 
+                        <Link to='/register'> Regístrate</Link>
+                    </p>
                 </section>
-                <p className="login__container--register">No tienes ninguna cuenta 
-                    <Link to='/register'> Regístrate</Link>
-                </p>
             </section>
-        </section>
-    </>
-)
+        </>
+    )
+}
 
 export default Login
