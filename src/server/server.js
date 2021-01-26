a8511f7aed30b881e45ee4a96e7b862a36d73557
Nico <nmanograsso@gmail.com>
2021-01-26 17:19:48 -0300

    add: config webpack dev and webpack hot mmiddlewares

--- src/server/server.js
+++ src/server/server.js
@@ -1,9 +1,21 @@
 import express from "express";
-import config from './config'
+import webpack from 'webpack';
+import config from './config';
 
 const { ENV, PORT } = config;
 
 const app = express()
+
+if (config.env === 'development') {
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = { port: PORT, hot: true };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
 
 app.get('*', (req, res) => {
     res.send({
