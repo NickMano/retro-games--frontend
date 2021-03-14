c8c13468837418fd8066d0e11037bbf2758895e3
Nico <nmanograsso@gmail.com>
2021-03-14 11:55:42 -0300

    update: eslint style

--- src/frontend/components/CarouselItem.jsx
+++ src/frontend/components/CarouselItem.jsx
@@ -1,47 +1,74 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { setFavorite, deleteFavorite } from '../actions'
-import '../assets/styles/components/CarouselItem.scss'
-import playIcon from '../images/icons8-circled-play-50.png'
-import plusIcon from '../images/icons8-add-50.png'
-import deleteIcon from '../images/icons8-remove-50.png'
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { setFavorite, deleteFavorite } from '../actions';
+import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../images/icons8-circled-play-50.png';
+import plusIcon from '../images/icons8-add-50.png';
+import deleteIcon from '../images/icons8-remove-50.png';
 
+const CarouselItem = (props) => {
+  const { id, cover, title, year, contentRating, isMyList } = props;
 
-const CarouselItem = props => {
-    const {id, cover, title, year, contentRating, isMyList} = props
-    
-    const handleSetFavorite = () => {
-        props.setFavorite(
-            {id, cover, title, year, contentRating}
-        )
-    }
+  const handleSetFavorite = () => {
+    props.setFavorite(
+      { id, cover, title, year, contentRating },
+    );
+  };
 
-    const handleDeleteFavorite = itemId => {
-        props.deleteFavorite(itemId)
-    }
+  const handleDeleteFavorite = (itemId) => {
+    props.deleteFavorite(itemId);
+  };
 
-    return(
-        <div className="carousel-item">
-            <img className="carousel-item__image" src={cover} alt={title} />
-            <div className="carousel-item__details">
-                <div>
-                    <img className="carousel-item__details--img" src={playIcon} alt="Play"/>
-                    {(isMyList === true)
-                    ? <img className="carousel-item__details--img" src={deleteIcon} alt="Delete Icon" onClick={() => handleDeleteFavorite(id)}/>
-                    : <img className="carousel-item__details--img" src={plusIcon} alt="Add Icon" onClick={handleSetFavorite}/>}
-                </div>
-                <p className="carousel-item__details--title"> {title}</p>
-                <p className="carousel-item__details--subtitle">
-                    {`${year} ${contentRating}`}
-                </p>
-            </div>
+  return (
+    <div className='carousel-item'>
+      <img className='carousel-item__image' src={cover} alt={title} />
+      <div className='carousel-item__details'>
+        <div>
+          <Link to={`/player/${id}`}>
+            <img className='carousel-item__details--img' src={playIcon} alt='Play' />
+          </Link>
+          {(isMyList === true) ? (
+            <img
+              className='carousel-item__details--img'
+              src={deleteIcon}
+              alt='Delete Icon'
+              onClick={() => handleDeleteFavorite(id)}
+            />
+          ) : (
+            <img
+              className='carousel-item__details--img'
+              src={plusIcon}
+              alt='Add Icon'
+              onClick={handleSetFavorite}
+            />
+          )}
         </div>
-    )
-}
+        <p className='carousel-item__details--title'>
+          {' '}
+          {title}
+        </p>
+        <p className='carousel-item__details--subtitle'>
+          {`${year} ${contentRating}`}
+        </p>
+      </div>
+    </div>
+  );
+};
 
-const mapDispatchToProps =  {
-    setFavorite,
-    deleteFavorite
-}
+CarouselItem.propTypes = {
+  title: PropTypes.string,
+  year: PropTypes.number,
+  contentRating: PropTypes.string,
+  cover: PropTypes.string,
+  deleteFavorite: PropTypes.func,
+  setFavorite: PropTypes.func,
+};
 
-export default connect(null, mapDispatchToProps)(CarouselItem)
+const mapDispatchToProps = {
+  setFavorite,
+  deleteFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
